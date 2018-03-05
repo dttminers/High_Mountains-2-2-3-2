4652895e1705b95a49667fdb7835c88056e08fd1
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2018-03-05 11:41:02 +0530

    Initial Commit

---
+++ High_MountainsTests/High_MountainsTests.swift
@@ -0,0 +1,36 @@
+//
+//  High_MountainsTests.swift
+//  High_MountainsTests
+//
+//  Created by Abhishek on 05/03/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+import XCTest
+@testable import High_Mountains
+
+class High_MountainsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
